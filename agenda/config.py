0f441791bfb46dde596b5aeb237d63c62d308d1e
rodrigoraa <rodrigoraa254@gmail.com>
2026-03-18 13:16:18 -0400

    Teste no servidor

--- agenda/config.py
+++ agenda/config.py
@@ -1,17 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
+# Pega o diretório onde este arquivo config.py está
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "chave_padrao_123")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Se não houver nada no .env, ele cria um banco local na pasta do projeto
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URI_LOCAL", "sqlite:///agendamento.db"
+        "DATABASE_URI_LOCAL", 
+        'sqlite:///' + os.path.join(basedir, 'agendamento.db')
     )
+    
     SQLALCHEMY_BINDS = {
-        "banco_externo": os.getenv(
-            "DATABASE_URI_EXTERNO", "sqlite:///banco_copiado_para_testes.db"
-        )
+        "banco_externo": os.getenv("DATABASE_URI_EXTERNO")
     }
